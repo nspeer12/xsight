ecfe5e6c142335bf24bd238db27537265285fe92
nspeer12 <nspeer252@gmail.com>
2020-04-07 17:33:14 -0400

    made everything oop and faster

--- fps.py
+++ fps.py
@@ -1,66 +1,59 @@
-from grabscreen import *
 import numpy as np
 import cv2
-import pyautogui
-from directkeys import PressKey, ReleaseKey, W, A, S, D
 from mouse import *
 import sys
 import time
-import imutils
-from cv2detection import ObjectDetection, Tracker
-
-
-def fps_counter(fps, print):
-    t = time.time()
-    fps = 1 / (t - fps)
-    if print:
-        sys.stdout.write("FPS  %.2f \r" % (fps) )
-    return t
-
-def status_update(text, print):
-    if print == True:
-        sys.stdout.write("\t\t  %s \r" % (text) )
+from vision import Vision
 
 
 def main():
     # x1, y1, x2, y2
     window = [0, 0, 1920, 1080]
-    
     # n defines the height and width of our square viewing region
-    n = 300
-    # view that the detection algo sees
+    n = 450
     roi = get_roi(window, n)
-    print(roi)
 
-    detection = ObjectDetection()
-    threshold = 0.7
-    
     printFps = True
     printStatus = False
+    
 
+    vision = Vision(window, roi, n)
     while(True):
-        t = time.time()
-
-        img = grab_screen(region=roi)
-        img, status, box = detection.detect(img)
-        t = fps_counter(t, printFps)
-        status_update("No Target", printStatus)
-
-        # taget acquired, begin tracking
-        if status == True:
-            objectRegion = get_coords(window, box, n)
-            tracker = Tracker(window, box, objectRegion, img, n)
-            tracker.lock_on()
-            # lock onto target
-            #lock_on(img, box, x1, y1, x2, y2, n) 
-            #status_update('Target Acquired', printStatus)
-            
-
-       # cv2.imshow('target', img)
-        # break loop
+        img =  vision.update_frame()
+        vision.single_detection()
+        cv2.imshow('detection', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+
+def get_roi(window, n):
+    n = n/2
+    # returns n by n region of interest in the center of the screen
+    xdiff = window[2] - window[0]
+    ydiff = window[3] - window[1]
+    xcenter = xdiff / 2
+    ycenter = ydiff / 2
+    roi =  [int(xcenter - n), int(ycenter - n), int(xcenter + n), int(ycenter + n)]
+    return roi
+
+
+def get_coords(window, objectRegion, n):
+    n = n/2
+    # returns the real location of the boxes relative to the gameplay screen
+    x1 = window[0]
+    y1 = window[1]
+    x2 = window[2]
+    y2 = window[3]
+
+    roixorgin = int(((x2 - x1) / 2) - n)
+    roiyorgin = int(((y2 - y1) / 2) - n)
+
+    objectRegion = [objectRegion[0] + roixorgin,
+                    objectRegion[1] + roiyorgin,
+                    objectRegion[2] + roixorgin,
+                    objectRegion[3] + roiyorgin]
+
+    return objectRegion
 
 if __name__ == '__main__':
     main()
