339a2bb479a3695609db3a5d76b6bbc63ba4a60b
nspeer12 <nspeer252@gmail.com>
2020-11-23 12:52:57 -0500

    created detection class

--- xsight/camera.py
+++ xsight/camera.py
@@ -2,39 +2,56 @@ import cv2
 import glob
 import numpy as np
 import os
+from detection import *
 
-# Specify device
-cap = cv2.VideoCapture(0)
 
 def take_photo(filename):
-	ret, frame = cap.read()
-	cv2.imwrite(filename, frame)
-
+    ret, frame = cap.read()
+    cv2.imwrite(filename, frame)
 
 def record_video(filename, mode = 'r'):
-	i = 0
-	while(True):
-		ret, frame = cap.read()
-		cv2.imshow('video', frame)
-		if( mode == 'r'):
-			cv2.imwrite(filename + str(i) + '.jpg', frame)
-			i += 1
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+    i = 0
+    while(True):
+        ret, frame = cap.read()
+        cv2.imshow('video', frame)
+        if( mode == 'r'):
+            cv2.imwrite(filename + str(i) + '.jpg', frame)
+            i += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+def real_time_detection():
+    d = Detection()
+
+    i = 0
+    while(True):
+        ret, frame = cap.read()
+        frame = d.object_detection(frame)
+
+        cv2.imshow('video', frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
 def write_to_video(dir, videoName):
-	img_arr = []
-	for filename in sorted(glob.glob(dir + '/*.jpg'), key = os.path.getmtime):
-		print(filename)
-		img = cv2.imread(filename)
-		img_arr.append(img)
+    img_arr = []
+    for filename in sorted(glob.glob(dir + '/*.jpg'), key = os.path.getmtime):
+        print(filename)
+        img = cv2.imread(filename)
+        img_arr.append(img)
 
-	height, width, layers = img_arr[0].shape
-	size = (width, height)
-	out = cv2.VideoWriter(videoName + '.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
+    height, width, layers = img_arr[0].shape
+    size = (width, height)
+    out = cv2.VideoWriter(videoName + '.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
 
-	for i in range(len(img_arr)):
-		out.write(img_arr[i])
+    for i in range(len(img_arr)):
+        out.write(img_arr[i])
 
-	out.release()
+    out.release()
+
+
+if __name__=="__main__":
+    # Specify device
+    cap = cv2.VideoCapture(0)
+    real_time_detection()
--- xsight/detection.py
+++ xsight/detection.py
@@ -9,10 +9,6 @@ import os
 import threading
 from multiprocessing import Pool
 
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-
-model.eval()
-
 # define the objects that we can detect
 COCO_INSTANCE_CATEGORY_NAMES = [
     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
@@ -30,82 +26,81 @@ COCO_INSTANCE_CATEGORY_NAMES = [
 ]
 
 
-def get_prediction(frame, threshold):
-	img = Image.open(frame) # Load the image
-	transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
-	img = transform(img) # Apply the transform to the images
-	pred = model([img]) # Pass the image to the model
-	pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction
-	pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
-	pred_score = list(pred[0]['scores'].detach().numpy())
 
-	# make predictions on the image
-	# this line of code is a bitch. Need to fix
-	try:
-		pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
-	except:
-		return None, None
+class Detection():
+    def __init__(self):
+        self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+        self.model.eval()
 
-	pred_boxes = pred_boxes[:pred_t+1]
-	pred_class = pred_class[:pred_t+1]
-	return pred_boxes, pred_class
+    def get_prediction(self, img, threshold):
+        transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
+        img = transform(img) # Apply the transform to the images
+        pred = self.model([img]) # Pass the image to the model
+        pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction
+        pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
+        pred_score = list(pred[0]['scores'].detach().numpy())
 
+        # make predictions on the image
+        # this line of code is a bitch. Need to fix
+        try:
+            pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
+        except:
+            return None, None
 
+        pred_boxes = pred_boxes[:pred_t+1]
+        pred_class = pred_class[:pred_t+1]
+        return pred_boxes, pred_class
 
-def object_detection(input_file, output_file, threshold=0.5, rect_th=1, text_size=1, text_th=1):
-	boxes, pred_cls = get_prediction(input_file, threshold) # Get predictions
-	img = input_file # Read image with cv2
-	#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
-	if boxes != None:
+    def object_detection(self, img, threshold=0.5, rect_th=1, text_size=1, text_th=1):
+        boxes, pred_cls = self.get_prediction(img, threshold) # Get predictions
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
+        if boxes != None:
             for i in range(len(boxes)):
                 cv2.rectangle(img, boxes[i][0], boxes[i][1],color=(0, 255, 0), thickness=rect_th) # Draw Rectangle with the coordinates
                 cv2.putText(img,pred_cls[i], boxes[i][0],  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) # Write the prediction class
-	plt.figure(figsize=(20,30)) # display the output image
-	plt.imshow(img)
-	plt.xticks([])
-	plt.yticks([])
-	plt.show()
-	write_detection(plt, output_file)
+        return img
+            
+    
 
 def write_detection(plt, filename):
-	plt.savefig(filename)
+    plt.savefig(filename)
 
 
 def object_detection_by_frame(filename, threshold=0.7, rect_th=2, text_size=2, text_th=2):
-	img = cv2.imread(filename)
-	boxes, pred_cls = get_prediction(filename, threshold)
+    img = cv2.imread(filename)
+    boxes, pred_cls = get_prediction(filename, threshold)
 
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-	if boxes != -1 or pred_cls != -1:
-		for i in range(len(boxes)):
-			cv2.rectangle(img, boxes[i][0], boxes[i][1], color = (0, 255, 0), thickness = rect_th)
-			cv2.putText(img, pred_cls[i], boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0, 255, 0), thickness = text_th)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    if boxes != -1 or pred_cls != -1:
+        for i in range(len(boxes)):
+            cv2.rectangle(img, boxes[i][0], boxes[i][1], color = (0, 255, 0), thickness = rect_th)
+            cv2.putText(img, pred_cls[i], boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0, 255, 0), thickness = text_th)
 
-	plt.figure(figsize=(16,9))
-	plt.imshow(img)
-	return plt
+    plt.figure(figsize=(16,9))
+    plt.imshow(img)
+    return plt
 
 
 def detect_all_frames(input, output):
-	i = 0
-	for filename in sorted(glob.glob(input + '/*.jpg'), key = os.path.getmtime):
-		print(filename)
-		plt = object_detection_by_frame(filename)
-		write_detection(plt, output + '/frame' + str(i) + '.jpg')
-		i += 1
-		plt.close()
+    i = 0
+    for filename in sorted(glob.glob(input + '/*.jpg'), key = os.path.getmtime):
+        print(filename)
+        plt = object_detection_by_frame(filename)
+        write_detection(plt, output + '/frame' + str(i) + '.jpg')
+        i += 1
+        plt.close()
 
 
 # detectes and writes to 'detected_output' directory
 def detect_and_write(filename):
-	print('detecting: ' , filename)
-	plt = object_detection_by_frame(filename)
-	write_detection(plt, filename)
-	plt.close()
-	print('detection complete: ' , filename)
+    print('detecting: ' , filename)
+    plt = object_detection_by_frame(filename)
+    write_detection(plt, filename)
+    plt.close()
+    print('detection complete: ' , filename)
 
 def threaded_detection(inputDir, outputDir):
-	i = 0
-	pool = Pool(processes = 1)
-	files = sorted(glob.glob(inputDir + '/*.jpg'), key = os.path.getmtime)
-	pool.map(detect_and_write, files)
+    i = 0
+    pool = Pool(processes = 1)
+    files = sorted(glob.glob(inputDir + '/*.jpg'), key = os.path.getmtime)
+    pool.map(detect_and_write, files)
